da8153bc52fbfc919b9b1231386346ae8bc9b8ff
layafakher <laya.fakher2000@gmail.com>
2020-09-14 12:37:15 +0430

    version1

---
+++ src/main/java/ir/ac/kntu/Main.java
@@ -0,0 +1,11 @@
+package ir.ac.kntu;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String []args){
+
+
+
+    }
+}
---
+++ src/main/java/ir/ac/kntu/WayToSend.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu;
+
+public enum WayToSend {
+    OVERLAND,
+    AIRWAY,
+    SEAWAY,
+}
